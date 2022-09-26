f6edbdc3df67c890fb73ca89f61b60b2b565d858
prakhil-tp <20636485+Prakhil-tp@users.noreply.github.com>
2022-09-26 08:20:10 +0530

    feat: export types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,4 @@
 export { ErrorPayload } from "./error-payload";
 export { AppContext } from "./app-context";
+export { TUser, TTodo } from "./entity-types";
+export { ServiceContext } from "./service-context";
