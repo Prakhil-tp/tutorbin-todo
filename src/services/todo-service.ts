b21e83b5d516b6b1e8936a3af3b68f54d51b7061
prakhil-tp <20636485+Prakhil-tp@users.noreply.github.com>
2022-09-26 08:20:44 +0530

    feat: Add todo-service

--- src/services/todo-service.ts
+++ src/services/todo-service.ts
@@ -1,1 +1,25 @@
-export class TodoService {}
+import { ServiceContext, TTodo } from "../types";
+
+export class TodoService {
+  context!: ServiceContext;
+
+  constructor(serviceContext: ServiceContext) {
+    this.context = serviceContext;
+  }
+
+  public async getTodo() {
+    return this.context.todoRepository.get();
+  }
+
+  public async addTodo(data: TTodo) {
+    return this.context.todoRepository.save(data);
+  }
+
+  public async updateTodo(id: string, data: TTodo) {
+    return this.context.todoRepository.update(id, data);
+  }
+
+  public async removeTodo(id: string) {
+    return this.context.todoRepository.remove(id);
+  }
+}
