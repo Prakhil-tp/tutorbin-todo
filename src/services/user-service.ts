75bf12d06469ca1f68cfdc1a9ced1ef6411f44bc
prakhil-tp <20636485+Prakhil-tp@users.noreply.github.com>
2022-09-26 08:09:45 +0530

    feat: Add user service

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,1 +1,24 @@
-export class UserService {}
+import { ServiceContext, TUser } from "../types";
+import jwt from "jsonwebtoken";
+
+export class UserService {
+  context!: ServiceContext;
+
+  constructor(serviceContext: ServiceContext) {
+    this.context = serviceContext;
+  }
+
+  public async getUser(email: string) {
+    return this.context.userRepository.get(email);
+  }
+
+  public async addUser(data: TUser) {
+    return this.context.userRepository.save(data);
+  }
+
+  public async generateUserToken(data: TUser) {
+    return jwt.sign(data, process.env.JWT_SECRET as string, {
+      expiresIn: "24h"
+    });
+  }
+}
